60ad08c715e72d9e257ff4e5a9a0db1ce13a21ac
Daniil Voshchuk <vochshukdaniil@gmail.com>
2022-12-15 18:46:41 +0300

    Fix bug with cors

--- packages/proxy/src/index.ts
+++ packages/proxy/src/index.ts
@@ -1,16 +1,28 @@
-import express, { json } from "express";
+import express, { json, Request, Response } from "express";
+import cors from "cors";
 
 import managementRouter from "~/api/management.routes";
 import usageRouter from "~/api/usage.routes";
 import config from "~/config";
-import ClientPicker from "~/helpers/ClientPicker";
 
 const app = express();
 
 app.use(json());
-ClientPicker(app);
+app.use(
+    cors({
+        credentials: true,
+        origin: config.client_url
+    })
+);
 app.use("/auth/manage", managementRouter());
 app.use("/proxy", usageRouter());
+if (config.mode === "production") {
+    app.use("/auth", express.static(__dirname + "/../../ui/build"));
+} else {
+    app.use("/auth", (_req: Request, res: Response) => {
+        res.redirect(config.client_url);
+    });
+}
 
 app.listen(config.port, () => {
     console.log(`Server started at port ${config.port}`);
