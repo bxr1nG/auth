7f9eeb4c969fafa7e8022d027a54fffc6d38d389
Daniil Voshchuk <vochshukdaniil@gmail.com>
2023-02-09 19:31:36 +0300

    fix(api/login): add headers to request

--- packages/ui/src/api/login.ts
+++ packages/ui/src/api/login.ts
@@ -3,7 +3,11 @@ import axios from "axios";
 import type FormikFields from "~/types/FormikFields";
 
 const login = async (values: FormikFields): Promise<FormikFields> => {
-    const response = await axios.post("/auth/manage/login", values);
+    const response = await axios.post("/auth/manage/login", values, {
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
 
     return response.data as FormikFields;
 };
