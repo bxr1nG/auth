036488461c9238535a55a82cc4684a447d04f34a
bxr1nG <vochshukdaniil@gmail.com>
2023-02-15 10:45:40 +0300

    feat(cross-var): add typescript
    re #1

---
+++ packages/cross-var/.eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+    env: {
+        es2021: true,
+        node: true
+    },
+    extends: [
+        "eslint:recommended",
+        "prettier",
+        "plugin:import/errors",
+        "plugin:import/warnings"
+    ],
+    overrides: [
+        {
+            files: ["*.ts", "*.tsx"],
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@typescript-eslint/recommended-requiring-type-checking"
+            ],
+            parserOptions: {
+                project: ["./tsconfig.json"],
+                tsconfigRootDir: __dirname
+            }
+        }
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    plugins: ["@typescript-eslint", "import"],
+    rules: {
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/consistent-type-imports": "error",
+        "import/no-unresolved": "off",
+        "import/order": [
+            "error",
+            {
+                groups: [
+                    "type",
+                    "builtin",
+                    "external",
+                    "internal",
+                    "parent",
+                    "index",
+                    "sibling",
+                    "object"
+                ],
+                pathGroups: [
+                    {
+                        pattern: "~/types/**",
+                        group: "parent",
+                        position: "before"
+                    },
+                    {
+                        pattern: "~/**",
+                        group: "parent",
+                        position: "before"
+                    },
+                    {
+                        pattern: "./**",
+                        group: "sibling",
+                        position: "before"
+                    }
+                ],
+                "newlines-between": "always",
+                distinctGroup: false
+            }
+        ]
+    }
+};
--- packages/cross-var/src/index.ts
+++ packages/cross-var/src/index.ts
@@ -1,16 +1,15 @@
-#!/usr/bin/env node --harmony
-
 import { exec } from "child_process";
+
 import spawn from "cross-spawn";
 import exit from "exit";
 
-function normalize(args) {
+function normalize(args: Array<string>): Array<string> {
     return args.map((arg) => {
         Object.keys(process.env)
             .sort((x, y) => x.length - y.length)
             .forEach((key) => {
                 const regex = new RegExp(`\\$${key}|%${key}%`, "ig");
-                arg = arg.replace(regex, process.env[key]);
+                arg = arg.replace(regex, process.env[key] as string);
             });
         return arg;
     });
@@ -19,18 +18,18 @@ function normalize(args) {
 let args = process.argv.slice(2);
 if (args.length === 1) {
     const [command] = normalize(args);
-    const proc = exec(command, (error, stdout, stderr) => {
+    const proc = exec(command as string, (error, stdout, stderr) => {
         if (error) {
-            console.error(`exec error: ${error}`);
+            console.error(`exec error: ${JSON.stringify(error, null, 2)}`);
             return;
         }
         process.stdout.write(stdout);
         process.stderr.write(stderr);
-        exit(proc.code);
+        exit(proc.exitCode as number);
     });
 } else {
     args = normalize(args);
     const command = args.shift();
-    const proc = spawn.sync(command, args, { stdio: "inherit" });
-    exit(proc.status);
+    const proc = spawn.sync(command as string, args, { stdio: "inherit" });
+    exit(proc.status as number);
 }
